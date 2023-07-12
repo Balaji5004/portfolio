0c5928fe5eb762505a68b79af316e8534916e212
Balaji5004 <grpbalaji5004@outlook.com>
2023-07-12 11:37:52 +0530

    changed hero section

--- src/Components/Home.js
+++ src/Components/Home.js
@@ -10,7 +10,7 @@ const Home = () => {
 
        <div className='max-w-screen-lg mx-auto flex flex-col items-center justify-center h-full px-4 md:flex-row '>
 
-          <div className='flex flex-col justify-center h-full'>
+          <div className='flex flex-col justify-center h-full text-center sm:text-left'>
             <h1 className='text-4xl sm:text-7xl font-bold text-white'> I'm P.Balaji </h1>
             <h3 className='text-2xl sm:text-4xl font-bold text-gray-300'>Full stack Web Developer | MERN</h3><br></br>
             <p className='text-gray-400 py-3 pe-4'>
@@ -34,7 +34,7 @@ const Home = () => {
           </div>
           
           <div>
-           <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-2/3 md:w-full'/>
+           <img src={HeroImage} alt='my profile' className='rounded-2xl mx-auto w-1/3 md:w-full'/>
           </div>
 
        </div>
--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -31,6 +31,7 @@ const NavBar = () => {
   ]
 
   return (
+    
     <div className="flex justify-between items-center w-full h-20 px-4 text-white bg-black fixed">
 
        <div>
